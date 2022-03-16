5d703fd8036481d94cabf12bb599ff953ca3a83c
Luiz Filipy <luizfilipy.aa@outlook.com>
2022-03-16 11:57:05 -0300

    u

--- test.sh
+++ test.sh
@@ -1,5 +1,5 @@
-repo_version=$(cd ../projetos/dev/bot-demo-with-ci-cd && git describe --tag)
-##repo_version="1.0"
+#repo_version=$(cd ../projetos/dev/bot-demo-with-ci-cd/ && git describe --tags $(git rev-list --tags --max-count=1))
+repo_version="1.0"
 #if java -jar botCLI.jar bot list --bid=botDemoCiCd | grep "Version: $repo_version" || [[ $? -eq 0 ]]; then
 #  echo "Versão encontrada: $repo_version"
 #  echo "DEPLOYED=1"
@@ -11,11 +11,11 @@ repo_version=$(cd ../projetos/dev/bot-demo-with-ci-cd && git describe --tag)
 if $repo_version && [[ $? -ne 0 ]]; then
   echo "nao passa"
 else
-#  repo_version=$(git describe --tags)
+#  repo_version=$(git describe --tags $(git rev-list --tags --max-count=1) || if [[ $? -eq 0 ]]; then exit 0; fi)
   if java -jar botCLI.jar bot list --bid=botDemoCiCd | grep "Version: $repo_version" || [[ $? -eq 0 ]]; then
-    echo "Versão encontrada: $repo_version"
+    echo "Já foi feito deploy"
+    echo $repo_version
   else
-    echo "Versão encontrada2: $repo_version"
+    echo "Fazer deploy"
   fi
 fi
-echo $?

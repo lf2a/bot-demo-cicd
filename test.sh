d46abbd05b6c7850bf1d25130c0021d5b7dca4cf
Luiz Filipy <luizfilipy.aa@outlook.com>
2022-03-16 11:11:18 -0300

    u

--- test.sh
+++ test.sh
@@ -1,5 +1,5 @@
-#repo_version=$(cd ../projetos/dev/bot-demo-with-ci-cd && git describe --tag)
-repo_version="1.0"
+repo_version=$(cd ../projetos/dev/bot-demo-with-ci-cd && git describe --tag)
+#repo_version="1.0"
 if java -jar botCLI.jar bot list --bid=botDemoCiCd | grep "Version: $repo_version" || [[ $? -eq 0 ]]; then
   echo "Versão encontrada: $repo_version"
   echo "DEPLOYED=1"
